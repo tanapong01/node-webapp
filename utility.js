91c09c898b324993c017b68874320f1b46d252e8
tanapong foochua <tanapong01>
2022-08-29 22:52:54 +0700

    codelab6

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5Mjc4MzgwLCJleHAiOjE2NTkyODE5ODAsIm5iZiI6MTY1OTI3ODM4MCwianRpIjoiVmFVTG5uV0toZXRWd2d5NyIsInN1YiI6MTMwLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.WvExTMB9GELGD8AMMs31mAOk4sddXEYpjTC5OeFUDGc"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzg3ODM0LCJleHAiOjE2NjE3OTE0MzQsIm5iZiI6MTY2MTc4NzgzNCwianRpIjoiODNOcDU0QXd5V2RyNXgyMSIsInN1YiI6MTMwLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.ibIqAluGm2D-HlNPxOrTObqY7Hz7bXMf9e3-3Aen7nE"; // put access token
 const student = {
   student_id: 63362686,
   name: 'Tanapong', // replace with your full name.
